59f9d539cf590e96bcc91be9beaa6ae268f04339
Andy <andy@wtribe.com>
2014-04-16 13:52:13 -0700

    Relicensed to Simplified BSD.

--- MuMech_Tools.cs
+++ MuMech_Tools.cs
@@ -1,10 +1,30 @@
-﻿// ToadicusTools © 2014 toadicus
+﻿// ToadicusTools
 //
-// This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-// To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+// MuMech_Tools.cs
 //
-//  This software uses methods derived from MuMechLib © 2013-2014 r4m0n, sarbian, et al
-//  Used under the terms of the General Public License, version 3.
+// Copyright © 2014, toadicus
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation and/or other
+//    materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// This software uses methods derived from MuMechLib © 2013-2014 r4m0n, sarbian, et al
+// Used under the terms of the General Public License, version 3.
 
 using System;
 using System.Collections.Generic;
