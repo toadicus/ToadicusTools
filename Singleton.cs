ad7c3b87f739dfc02529b49376a1b248b1495937
Andy Wilkinson <andy@wtribe.com>
2014-07-04 15:23:39 -0700

    Singleton: Probably more thread-safe now.

--- Singleton.cs
+++ Singleton.cs
@@ -33,17 +33,12 @@ namespace ToadicusTools
 	public abstract class Singleton<T>
 		where T : class
 	{
-		private static T _instance;
+		private static T _instance = Activator.CreateInstance(typeof(T), true) as T;
 
 		public static T Instance
 		{
 			get
 			{
-				if (_instance == null)
-				{
-					_instance = Activator.CreateInstance(typeof(T), true) as T;
-				}
-
 				return _instance;
 			}
 		}
